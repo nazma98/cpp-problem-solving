a4eef760affe5927a32e53b974d2c5c12b1e9139
nazma98 <nazmasarker1311@gmail.com>
2024-11-16 15:27:27 +0600

    Add codeforces phone number cpp file

---
+++ cf-phone-number.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num_card;
+    cin>>num_card;
+    string digits;
+    cin>>digits;
+
+    int len = sizeof(digits);
+    cout<<len<<"\n";
+    return 0;
+}
