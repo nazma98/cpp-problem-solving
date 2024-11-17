a7c7abf0d66c312af1479426cc74952fb8e76a84
nazma98 <nazmasarker1311@gmail.com>
2024-11-17 15:31:22 +0600

    Calculate frequency array of cses missing number

--- cses-problems/missing-number.cpp
+++ cses-problems/missing-number.cpp
@@ -1,7 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int N = 1e5 + 10;
+int arr[N];
+int freq[N];
+
 int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n-1; i++) {
+        cin>>arr[i];
+ 
+    }
+
+
+    for(int i=0; i<n; i++) {
+        freq[arr[i]]++;
+    }
+
+        for(int i=1 ; i<=n; i++) {
+        cout<<freq[i]<<endl;
     
+    }
+
+    for(int i=1; i<=n; i++){
+        if(freq[i] == 0) {
+            cout<<freq[i]<<endl;
+            break;
+        }
+    }
     return 0;
 }
