a044bfe137cb0eaa1eef3eeb44c7b12ec110b599
nazma98 <nazmasarker1311@gmail.com>
2024-11-17 12:31:03 +0600

    Compute prefix sum of array

---
+++ sum-subarray-query.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+int ar[N];
+int pr[N];
+int main(){
+    int ar[] = {3, 6, 2 ,8, 9, 2};
+    int n = sizeof(ar) / sizeof(ar[0]);
+
+    // calculate prefix sum
+    pr[0] = ar[0];
+    for(int i=1; i<n; i++){
+        pr[i] = pr[i-1] + ar[i];
+    }
+
+    for(int i=0; i<n; i++) {
+        cout<<pr[i]<<" ";
+    }
+
+    cout<<endl;
+    return 0;
+}
