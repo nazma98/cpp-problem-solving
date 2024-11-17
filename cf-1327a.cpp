d9bc82b748d055f6bc90f53590714d6bcdce7a47
nazma98 <nazmasarker1311@gmail.com>
2024-11-17 11:33:47 +0600

    Add cpp file of codeforces problem 1327a

---
+++ cf-1327a.cpp
@@ -0,0 +1,20 @@
+// link to problem https://codeforces.com/problemset/problem/1327/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+int main() {
+    fast_io; // Enable fast input and output
+    
+    int t;
+    cin >> t; // Input the number of test cases
+    while (t--) {
+        int n;
+        cin >> n; // Example input for a single test case
+        cout << n * n << "\n"; // Example output
+    }
+
+    return 0;
+}
