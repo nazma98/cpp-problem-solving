93c08c3cce9ee20eb17e2e835181cae8c267ca46
nazma98 <nazmasarker1311@gmail.com>
2024-11-18 12:12:48 +0600

    Add even odd numbers

--- cses-problems/permutations.cpp
+++ cses-problems/permutations.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 
 int main(){
-    
+    long long n, i;
+        cin>>n;
+    vector<long long> even(n), odd(n);
+
+
+    if(n < 4){
+        cout<<"NO SOLUTION"<<endl;
+    } else {
+        for(i=1; i<n; i++){
+            if(i%2 != 0){
+                odd.push_back(i);
+            } else {
+                even.push_back(i);
+            }
+        }
+
+        for(i=1; i<n; i++){
+            cout<<"Even "<<even[i];
+        }
+
+    }
 
 
     return 0;
