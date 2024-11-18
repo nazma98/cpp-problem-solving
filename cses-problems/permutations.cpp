a75ea1f803420e1d68dabffc04a889b29ad2dd1e
nazma98 <nazmasarker1311@gmail.com>
2024-11-18 18:16:10 +0600

    Add solution to cses permutations

--- cses-problems/permutations.cpp
+++ cses-problems/permutations.cpp
@@ -1,29 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     long long n, i;
-        cin>>n;
-    vector<long long> even(n), odd(n);
+    cin >> n;
 
-
-    if(n < 4){
-        cout<<"NO SOLUTION"<<endl;
+    if (n < 4) {
+        cout << "NO SOLUTION" << endl;
     } else {
-        for(i=1; i<n; i++){
-            if(i%2 != 0){
-                odd.push_back(i);
-            } else {
+        vector<long long> even, odd; // No pre-allocation
+
+        for (i = 1; i <= n; i++) { // Loop must go up to n
+            if (i % 2 == 0) {
                 even.push_back(i);
+            } else {
+                odd.push_back(i);
             }
         }
 
-        for(i=1; i<n; i++){
-            cout<<"Even "<<even[i];
-        }
+        for (auto x : even) cout << x << " ";
+        for (auto x : odd) cout << x << " ";
 
+        cout << endl;
     }
-
 
     return 0;
 }
