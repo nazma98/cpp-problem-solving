332f04a27a7da9abcd009d2fc42e5a4833944ef8
nazma98 <nazmasarker1311@gmail.com>
2024-11-23 19:46:20 +0600

    Insert element to the vector

--- insert_n_erase.cpp
+++ insert_n_erase.cpp
@@ -1,1 +1,22 @@
 // Create a vector of integers, insert 5 at the second position, and remove the third element. Print the vector.
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    vector<int> v = {2,4,6,8,10,12};
+
+    v.insert(v.begin()+1, 5);
+
+    for(auto it = v.begin(); it!= v.end(); it++) {
+        cout<<*it<<" ";
+    }
+
+    cout<<"\n";
+
+    return 0;
+    
+} 
+
+
