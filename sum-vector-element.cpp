3edcdf117465a690d02ad7c78e16a9a656206e20
nazma98 <nazmasarker1311@gmail.com>
2024-11-23 19:18:32 +0600

    Add cpp program to sum vector elements

---
+++ sum-vector-element.cpp
@@ -0,0 +1,11 @@
+// Write a function that takes a vector of integers as input and returns the sum of its elements.
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    vector<int>v;
+    
+
+    return 0;
+}
