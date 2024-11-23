708ec0e92a36f92e519d63a9bc0459f0cfc8d67f
nazma98 <nazmasarker1311@gmail.com>
2024-11-23 12:00:30 +0600

    Add cpp pointers exercise

---
+++ hackerrank-pointers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num = 10;
+    int *p = &num;
+    cout<<p<<endl;
+    cout<<*p<<endl;
+    cout<< *p + 1<<endl;
+
+    return 0;
+}
