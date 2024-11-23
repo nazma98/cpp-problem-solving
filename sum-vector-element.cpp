fa6e218e46a69ec0f01e7e20af9b11a6737944b1
nazma98 <nazmasarker1311@gmail.com>
2024-11-23 19:37:21 +0600

    Add function to sum vector elements

--- sum-vector-element.cpp
+++ sum-vector-element.cpp
@@ -2,10 +2,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int input_sum(vector<int>& v){
+    int n;
+    cin>>n;
+    int num;
+    for(int i=0; i<n; i++){
+        cin>> num;
+        v.push_back(num);
+    }
+
+    int sum = 0;
+
+    for(int i=0; i<n; i++){
+        sum += v[i];
+    }
+
+    return sum;
+}
 
 int main(){
     vector<int>v;
-    
 
+    int sum = input_sum(v);
+    cout<<sum<<"\n";
     return 0;
 }
