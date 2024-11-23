e91e0a215928c5a4754053c8bbcc175d6e56fc55
nazma98 <nazmasarker1311@gmail.com>
2024-11-23 12:56:03 +0600

    Add equilateral method

--- hackerRank-problems/hr-multilevel-inheritence.cpp
+++ hackerRank-problems/hr-multilevel-inheritence.cpp
@@ -21,7 +21,10 @@ class Isosceles : public Triangle{
 
 //Write your code here.
 class Equilateral:public Isosceles{
-
+    public:
+        void equilateral(){
+            cout<<"I am an equilateral triangle\n";
+        }
 };
 
 int main(){
