2b21f9c3a4aab02ed2a7697f314151d991ee912b
nazma98 <nazmasarker1311@gmail.com>
2024-11-23 19:55:09 +0600

    Add input methods

--- reverse-vector.cpp
+++ reverse-vector.cpp
@@ -2,8 +2,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+vector<int> reverseVector(vector<int>& v) {
+    
+}
 
+int main(){
+    vector<int>v;
+
+    int n;
+    cin>>n;
+    int num;
+    for(int i=0; i<n; i++) {
+
+        cin>>num;
+        v.push_back(num);
+    }
+
+    reverseVector(v);
 
 
     return 0;
