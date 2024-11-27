953bfce56ca5333a9ba0f77721b3eae293b37f3d
nazma98 <nazmasarker1311@gmail.com>
2024-11-27 21:14:17 +0600

    Print count of apples

--- hash-table-prac.cpp
+++ hash-table-prac.cpp
@@ -5,5 +5,6 @@ int main(){
     unordered_map<string, int> wordcount;
     wordcount["apple"] = 1;
     wordcount["banana"] = 3;
+    cout<<"Count of apples "<<wordcount["apple"]<<"\n";
     return 0;
 }
