d79cbfef3ea98c06aa8b84b6de142ff7f9a287e6
nazma98 <nazmasarker1311@gmail.com>
2024-11-27 21:24:12 +0600

    Add cpp file to find most frequent number

---
+++ most-frequent-element.cpp
@@ -0,0 +1,4 @@
+// Problem: Find the Most Frequent Element
+// Given an array of integers, find the element that occurs most frequently.
+// If there are multiple elements with the same highest frequency, return any one of them.
+
