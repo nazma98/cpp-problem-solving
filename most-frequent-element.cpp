f851e4db30465df61649957a21dec3fbc48bd4fc
nazma98 <nazmasarker1311@gmail.com>
2024-11-27 21:27:23 +0600

    Add unordered_map

--- most-frequent-element.cpp
+++ most-frequent-element.cpp
@@ -1,4 +1,12 @@
 // Problem: Find the Most Frequent Element
 // Given an array of integers, find the element that occurs most frequently.
 // If there are multiple elements with the same highest frequency, return any one of them.
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    unordered_map<int, int> frequncy_map;
+    
+
+    return 0;
+}
