adbce5e1cbbbe5f4bcdc67a7024f13d000b253ca
nazma98 <nazmasarker1311@gmail.com>
2024-12-03 20:00:16 +0600

    Add function to perform bfs

--- bfs-prac.cpp
+++ bfs-prac.cpp
@@ -1,6 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void bfs(vector<vector<int>> &graph, int start){
+    vector<bool>visited(graph.size(), false);
+    queue<int>q;
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()){
+        int node = q.front();
+        q.pop();
+        cout<<node<<" ";
+
+        for(int neighbor : graph[node]) {
+            if(!visited[neighbor]){
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
 int main(){
     vector<vector<int>>graph={{0, 1, 2},
     {1, 2},
