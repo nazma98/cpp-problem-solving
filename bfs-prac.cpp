17cb464e27a4d64ad317560cf2d503414f901861
nazma98 <nazmasarker1311@gmail.com>
2024-12-03 19:52:26 +0600

    Add the graph

--- bfs-prac.cpp
+++ bfs-prac.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main(){
-
+    vector<vector<int>>graph={{0, 1, 2},
+    {1, 2},
+    {0, 3, 4},
+    {0, 5, 6},
+    {1},
+    {1},
+    {2},
+    {2}
+    };
 
     return 0;
 }
