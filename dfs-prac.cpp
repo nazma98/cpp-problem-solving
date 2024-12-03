bb331fdb9b09d9c7f5b69d3aa560e9519786267e
nazma98 <nazmasarker1311@gmail.com>
2024-12-03 21:36:51 +0600

    Add dfs problem cpp

---
+++ dfs-prac.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dfsRecursive(vector<vector<int>> &graph, int node) {
+    vector<bool>visited(graph.size(), false);
+    visited[node] = true;
+    cout<<node<<" ";
+    
+    for(int neighbor : graph[node]) {
+        if(!visited[neighbor]){
+            dfsRecursive(graph, neighbor);
+        }
+    }
+}
+
+int main(){
+    vector<vector<int>>graph = {
+    {0, 1, 2},
+    {1, 2},
+    {0, 3, 4},
+    {0, 5, 6},
+    {1},
+    {1},
+    {2},
+    {2}
+    };
+
+    return 0;
+}
