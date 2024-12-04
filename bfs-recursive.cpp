9f77ec1a8573e2a48cb32390a57dc1e47c9713f7
nazma98 <nazmasarker1311@gmail.com>
2024-12-04 23:46:17 +0600

    Add example

--- bfs-recursive.cpp
+++ bfs-recursive.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main(){
-
+    vector<vector<int>> graph = {
+        {1, 2},        // Neighbors of node 0
+        {0, 3, 4},     // Neighbors of node 1
+        {0, 5},        // Neighbors of node 2
+        {1},           // Neighbors of node 3
+        {1},           // Neighbors of node 4
+        {2}            // Neighbors of node 5
+    };
 
 
     return 0;
