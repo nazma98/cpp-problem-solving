40390c847094f0e4d75c3e438da5485b5d987f64
nazma98 <nazmasarker1311@gmail.com>
2024-12-05 19:36:36 +0600

    Add bfsRecursiveHelper function

--- bfs-recursive.cpp
+++ bfs-recursive.cpp
@@ -1,6 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void bfsRecursiveHelper(vector<vector<int>> &graph, vector<bool> &visited, queue<int> &q) {
+    if(q.empty()) {
+        return;
+    }
+
+    int node = q.front();
+    q.pop();
+    cout<<"Visited node "<<node<<endl;
+
+    for(int neighbor: graph[node]){
+        if(!visited[neighbor]) {
+            visited[neighbor] = true;
+            q.push(neighbor);
+        }
+    }
+    bfsRecursiveHelper(graph, visited, q);
+}
+
 void bfsRecursive(vector<vector<int>> &graph, int start) {
     vector<bool> visited(graph.size(), false);
     queue<int>q;
