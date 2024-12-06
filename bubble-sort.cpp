75f183993028fd03b32a325d440a13a4c39c6bdc
nazma98 <nazmasarker1311@gmail.com>
2024-12-06 19:45:24 +0600

    Add bubble sort function

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -2,13 +2,27 @@
 using namespace std;
 
 void bubbleSort(int arr[], int size){
-
+    int temp;
+    for(int i=0; i < size; i++){
+        for(int j = 1; j < size - i +1; j++){
+            if(arr[j-1] > arr[j]) {
+                temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
 
 int main(){
     int arr[] = {2, 4, 10, 8, 3 , 12, 9};
     int len = sizeof(arr) / sizeof(arr[0]);
     bubbleSort(arr, len);
+
+    for(int i=0 ; i< len; i++) {
+        cout<<arr[i] <<" ";
+    }
+    cout<<endl;
 
     return 0;
 }
