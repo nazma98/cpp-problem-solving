bc0380b6d852a290608c573eafc5b04c48455779
nazma98 <nazmasarker1311@gmail.com>
2024-12-06 19:39:05 +0600

    Add bubble sort cpp file

---
+++ bubble-sort.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int size){
+
+}
+
+int main(){
+    int arr[] = {2, 4, 10, 8, 3 , 12, 9};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, len);
+
+    return 0;
+}
