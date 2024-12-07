ceb91e5004e72433f5fcba58aa1511ba1025deee
nazma98 <nazmasarker1311@gmail.com>
2024-12-07 22:52:23 +0600

    Add ascending sort method

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -15,7 +15,7 @@ int main(){
     for(int i=1; i<n; i++) {
         temp = arr[i];
         j = i - 1;
-        while(temp > arr[j] && j >= 0){
+        while(temp < arr[j] && j >= 0){
             arr[j+1] = arr[j];
             j--;
         }
