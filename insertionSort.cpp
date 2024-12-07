9feaf7bb72fe563932adc46a07f879dcf3840144
nazma98 <nazmasarker1311@gmail.com>
2024-12-07 22:49:31 +0600

    Add insertion sort method

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -10,5 +10,22 @@ int main(){
         cin>>arr[i];
     }
 
+    int temp, j;
+
+    for(int i=1; i<n; i++) {
+        temp = arr[i];
+        j = i - 1;
+        while(temp > arr[j] && j >= 0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
     return 0;
 }
