d9c3252964091b3094ebb21f3cf880093b1c2bca
nazma98 <nazmasarker1311@gmail.com>
2024-12-11 19:31:08 +0600

    Add gcd recursive function

--- lcm.cpp
+++ lcm.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int a, int b){
+    if(b == 0) return a;
+    return gcd(b, a%b);
+}
+
 int main(){
     int a, b;
     cin>>a>>b;
+    int lcm = (a*b) / gcd(a, b);
     return 0;
 }
