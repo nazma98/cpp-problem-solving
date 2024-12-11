f9a019da87d4dc401af7b537ede34fc6055283a6
nazma98 <nazmasarker1311@gmail.com>
2024-12-11 18:53:45 +0600

    Add gcd recursive function

--- gcd-recursive.cpp
+++ gcd-recursive.cpp
@@ -1,7 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int gcdRecursive(int a, int b) {
+    if(b == 0) {
+        return a;
+    }
+
+    return gcdRecursive(b, a%b);
+}
 int main(){
-    
+    int a, b;
+    cin>>a>>b;
+    cout<<gcdRecursive(a, b);    
     return 0;
 }
