524409118a5bd55ce157937536331ee15c9ad101
nazma98 <nazmasarker1311@gmail.com>
2024-12-11 15:44:31 +0600

    Add gcd funtion

--- gcd.cpp
+++ gcd.cpp
@@ -1,9 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int gcd(int a, int b){
+    int temp;
+
+    while (b!=0)
+    {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+    
+}
+
 int main(){
     int a, b;
     cin>>a>>b;
-    
+
+    int result = gcd(a, b);
+    cout<<"GCD of "<<a<<" and "<<b<<" is "<<result<<"\n";
     return 0;
 }
