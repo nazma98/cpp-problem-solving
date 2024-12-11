b4f1d46f3bd9e9271c7e4a3755402c1bf113bdf7
nazma98 <nazmasarker1311@gmail.com>
2024-12-11 15:45:13 +0600

    Format code

--- gcd.cpp
+++ gcd.cpp
@@ -1,24 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b){
+int gcd(int a, int b)
+{
     int temp;
 
-    while (b!=0)
+    while (b != 0)
     {
         temp = b;
         b = a % b;
         a = temp;
     }
     return a;
-    
 }
 
-int main(){
+int main()
+{
     int a, b;
-    cin>>a>>b;
+    cin >> a >> b;
 
     int result = gcd(a, b);
-    cout<<"GCD of "<<a<<" and "<<b<<" is "<<result<<"\n";
+    cout << "GCD of " << a << " and " << b << " is " << result << "\n";
     return 0;
 }
