ee5a5e0741c55cadec76bf270ecee9fccda21dcd
nazma98 <nazmasarker1311@gmail.com>
2024-12-12 22:09:10 +0600

    Add gcd function

--- gcd-of-array.cpp
+++ gcd-of-array.cpp
@@ -2,7 +2,25 @@
 #include<vector>
 using namespace std;
 
-int main(){
+int gcd(int a, int b) {
+    if(b == 0) {
+        return a;
+    }
+    return gcd(b, a%b);
+}
 
+int main(){
+    int num;
+    cin>>num;
+    vector<int>nums(num+1);
+    for(int i=0; i<num; i++) {
+        cin>>nums[i];
+    }
+
+    int result = nums[0];
+    for(int i=1; i<num; i++) {
+        result = gcd(result, nums[i]);
+    }
+    cout<<result<<endl;
     return 0;
 }
