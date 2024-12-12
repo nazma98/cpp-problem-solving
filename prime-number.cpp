7f0a247081d423ace9ee5490b15ddc890407d92b
nazma98 <nazmasarker1311@gmail.com>
2024-12-12 21:33:42 +0600

    Add isprime check function

--- prime-number.cpp
+++ prime-number.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
+bool isPrime(int num){
+    for(int i = 2; i*i <= num; i++) {
+        if(num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(){
     int num;
     cin>>num;
-
+    
+    if(isPrime(num)){
+        cout<<num<<" is prime\n";
+    } else {
+        cout<<num<<" is not prime\n";
+    }
     return 0;
 }
