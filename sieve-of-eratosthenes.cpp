e3e14e63a98c025d11b0695e3a11fe1bf3fb2315
nazma98 <nazmasarker1311@gmail.com>
2024-12-12 21:52:42 +0600

    Add sieve of eratosthenes function

--- sieve-of-eratosthenes.cpp
+++ sieve-of-eratosthenes.cpp
@@ -2,6 +2,17 @@
 #include<vector>
 using namespace std;
 
+void sieveOfEratosthenes(vector<bool>&prime, int num) {
+    prime[0] = prime[1] = false;
+    for(int i=2; i*i <= num; i++) {
+        if(prime[i]){
+            for(int j = i*i; j<=num; j+= i) {
+                prime[j] = false;
+            }
+        }
+    }
+}
+
 int main(){
     int num;
     cin>>num;
